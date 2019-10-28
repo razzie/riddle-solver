4634a0bdf961c8341719201eb288048316237390
Gábor Görzsöny <gabor@gorzsony.com>
2019-10-28 03:52:26 +0100

    Fixed highlight coloring of rules

--- ui/util.go
+++ ui/util.go
@@ -8,12 +8,11 @@ import (
 )
 
 func colorize(text string) string {
-	primaryColor := tview.Styles.PrimaryTextColor.Hex()
-	secondaryColor := tview.Styles.SecondaryTextColor.Hex()
-	return fmt.Sprintf("[#%06x]%s[#%06x]", secondaryColor, text, primaryColor)
+	color := tview.Styles.SecondaryTextColor.Hex()
+	return fmt.Sprintf("[#%06x]%s[-]", color, text)
 }
 
 func colorizeItem(item riddle.Item) string {
 	itemType, value := item.Split()
-	return fmt.Sprintf("%s:%s", itemType, colorize(value))
+	return fmt.Sprintf("[-]%s:%s", itemType, colorize(value))
 }
