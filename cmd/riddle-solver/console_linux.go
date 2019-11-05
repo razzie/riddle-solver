7b066e83ab2af32a5088f044b249a4e344a23a49
Gábor Görzsöny <gabor@gorzsony.com>
2019-11-05 03:29:20 +0100

    Fixed title on Linux

--- cmd/riddle-solver/console_linux.go
+++ cmd/riddle-solver/console_linux.go
@@ -7,6 +7,6 @@ import (
 
 // SetConsoleTitle sets the console title
 func SetConsoleTitle(title string) error {
-	fmt.Printf("\033]0;Title goes here\007")
+	fmt.Printf("\033]0;%s\007", title)
 	return nil
 }
