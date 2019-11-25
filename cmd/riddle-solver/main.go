0e60d691f323d90d5aa4fe5ec03439cb79d6c3b1
Gábor Görzsöny <gabor@gorzsony.com>
2019-11-25 03:52:10 +0100

    Moved riddles to riddles folder

--- cmd/riddle-solver/main.go
+++ cmd/riddle-solver/main.go
@@ -12,7 +12,7 @@ func main() {
 	demo := flag.Bool("demo", false, "Einstein's 5 house riddle demo mode")
 	theme := flag.String("theme", "light", "Specify light or dark theme")
 	debug := flag.Bool("debug", false, "Enable an additional debug page")
-	load := flag.String("load", "autosave.json", "Specify a riddle JSON file to load")
+	load := flag.String("load", "riddles/autosave.json", "Specify a riddle JSON file to load")
 	flag.Parse()
 
 	if t, ok := ui.Themes[*theme]; ok {
@@ -38,6 +38,6 @@ func main() {
 
 	r, err := app.GetRiddle()
 	if err == nil {
-		r.SaveToFile("autosave.json")
+		r.SaveToFile("riddles/autosave.json")
 	}
 }
