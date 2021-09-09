dfd929a51f6357ee0a1381dbf95eb0fdf35cac56
Gábor Görzsöny <gabor@gorzsony.com>
2021-09-10 00:30:26 +0200

    Disabled gui theme flag until more themes are added

--- cmd/riddle-solver-gui/main.go
+++ cmd/riddle-solver-gui/main.go
@@ -9,13 +9,13 @@ import (
 )
 
 var (
-	theme      string
+	//theme      string
 	riddleFile string
 	debug      bool
 )
 
 func init() {
-	flag.StringVar(&theme, "theme", "light", "Specify light or dark theme")
+	//flag.StringVar(&theme, "theme", "light", "Specify light or dark theme")
 	flag.StringVar(&riddleFile, "load", "", "Specify a riddle JSON file to load")
 	flag.BoolVar(&debug, "debug", false, "Enable an additional debug page")
 	flag.Parse()
@@ -33,12 +33,12 @@ func tryLoadRiddle() *riddle.Riddle {
 }
 
 func main() {
-	t, ok := gui.Themes[theme]
+	/*t, ok := gui.Themes[theme]
 	if !ok {
 		log.Fatalf("Theme not found: %s", theme)
-	}
+	}*/
 
-	app := gui.NewApp(t, debug)
+	app := gui.NewApp(gui.LightTheme, debug)
 	if r := tryLoadRiddle(); r != nil {
 		app.SetRiddle(r)
 	}
